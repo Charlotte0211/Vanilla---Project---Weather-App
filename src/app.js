20c9997bcbfac4279538d740f4f45055f4e19857
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2021-02-21 20:23:23 +0000

    Added relevant weather icon

---
+++ src/app.js
@@ -0,0 +1,25 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector('#temperature');
+  let cityElement = document.querySelector('#city');
+  let descriptionElement = document.querySelector('#description');
+  let humidityElement = document.querySelector('#humidity');
+  let windElement = document.querySelector('#wind');
+  let iconElement = document.querySelector('#icon');
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    'src',
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute('alt', response.data.weather[0].description);
+}
+
+let apiKey = 'e1c78352c85e71ca99730b10f46fa658';
+let city = 'Lisbon';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);

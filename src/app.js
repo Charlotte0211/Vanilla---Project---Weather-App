a5b9845b3f775792163d33a2255dc2ff3611d2c0
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2021-04-18 19:06:39 +0100

    Weather forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector('#temperature');
   let cityElement = document.querySelector('#city');
   let descriptionElement = document.querySelector('#description');
@@ -44,27 +43,35 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute('alt', response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = 'e1c78352c85e71ca99730b10f46fa658';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector('#forecast');
 
   let forecastHTML = `<div class="row">`;
-  let days = ['Mon', 'Tues', 'Wed'];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay) {
+    forecastHTML += `
   <div class="col-2">
-  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-date">${forecastDay.dt}</div>
   <img
-  src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
+  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
   alt=""
   width="42"
   />
   <div class="weather-forecast-temperatures>
-  <span class="weather-forecast-temperature-max>18°</span>
-  <span class="weather-forecast-temperature-min12°</span>
+  <span class="weather-forecast-temperature-max>${forecastDay.temp.max}°</span>
+  <span class="weather-forecast-temperature-min>${forecastDay.temp.min}°</span>
   </div>
   </div>
   `;
@@ -118,4 +125,3 @@ let celsiusLink = document.querySelector('#celsius-link');
 celsiusLink.addEventListener('click', displayCelsiusTemperature);
 
 search('Edinburgh');
-displayForecast();
